36265a614085c708dae2d68266710b0c00c47e7f
Ayushsaini03 <114101553+Ayushsaini03@users.noreply.github.com>
2023-02-14 03:45:07 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -108,3 +108,28 @@ function CheckDay(day){
         document.getElementById("day" + (i+1)).innerHTML = weekday[CheckDay(i)];
     }
     
+function changebg(){
+    const images = [
+        'url("1.jpg")',
+        'url("2.jpg")',
+        'url("3.jpg")',
+        'url("4.jpg")',
+        'url("5.jpg")',
+        'url("6.jpg")',
+        'url("7.jpg")',
+        'url("8.jpg")',
+        'url("9.jpg")',
+        'url("10.jpg")',
+        'url("11.jpg")',
+        'url("12.jpg")',
+        'url("13.jpg")',
+
+        
+    ]
+    const section = document.querySelector('section')
+    const bg = images[Math.floor(Math.random()*
+        images.length)];
+        section.style.backgroundImage = bg;
+}
+
+setInterval(changebg,15000)
